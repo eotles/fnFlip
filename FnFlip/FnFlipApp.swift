d797513314315f11b36cc0a0b9cfc80d0148cd3a
Erkin Otles <eotles@gmail.com>
2025-10-01 09:00:43 -0500

    Initial Commit

---
+++ FnFlip/FnFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  FnFlipApp.swift
+//  FnFlip
+//
+//  Created by Erkin Otles on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct FnFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
